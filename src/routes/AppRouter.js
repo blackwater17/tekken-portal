466deda68e66f3af1a22ea3ee2b34d5c09f88be3
blackwater17 <dextermorgan_7@hotmail.com>
2024-04-21 17:24:34 +0300

    update

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,23 +1,10 @@
-import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import Character from '../pages/Character';
 import NotFoundPage from '../pages/NotFoundPage';
 
 function AppRouter() {
   return (
-   
-    /*
-    <BrowserRouter>
-      <div className='site-container'>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/character/:character_id/:character_name" element={<Character />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-    */
-
     <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
